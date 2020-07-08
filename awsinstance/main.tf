284a1dd2951ac5acff8bcb9b053429d25e413a86
Moff <jfrancisco.poo@gmail.com>
2020-07-08 18:56:16 -0500

    Test001

--- awsinstance/main.tf
+++ awsinstance/main.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+    version = "~> 2.0"
     region="us-east-1"
 }
 
@@ -8,25 +9,24 @@ variable "instance_type" {
 }
 
 variable "department" {
-  description = "vRA"
+  description = "Department tag"
+}
+
+resource "random_string" "random" {
+    length = 16
+    special = true
+    override_special = "/@£$"
+
+    provisioner "local-exec" {
+       command = "echo sleeping for 10 mins; sleep 600; echo slept"
+    }
 }
 
 resource "aws_instance" "machine1" {
+    depends_on = [
+        random_string.random,
+       ]
     ami           = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
-
-    tags = {
-        department = var.department
-    }
-}
-
-resource "aws_instance" "machine2" {
-    ami           = "ami-04b9e92b5572fa0d1"
-    instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
-
-    tags = {
-        department = var.department
-    }
+    availability_zone = "us-east-1b"
 }
