74794c99c4b6135986f726b9bd1e2c2e2ab6a5e5
Moff <jfrancisco.poo@gmail.com>
2020-07-08 22:30:20 -0500

    Manual-v005

--- awsinstance/main.tf
+++ awsinstance/main.tf
@@ -1,30 +1,23 @@
-# Create a new instance of the latest Ubuntu 14.04 on an
-# t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
-  region = "us-east-1"
+    version = "2.69.0"
+    region="us-east-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
 }
 
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
+variable "myTag" {
+  description = "My Input Tag"
+  default = "terraform-test"
+}
 
-  tags = {
-    Name = "HelloWorld"
-  }
+resource "aws_instance" "machine1" {
+    ami           = "ami-04b9e92b5572fa0d1"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1b"
+    tags = {
+      "type" = var.myTag
+    }
 }
