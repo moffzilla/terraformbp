169317a4d639b8ca8549d1ce1fc44a16865aaa36
Moff <jfrancisco.poo@gmail.com>
2020-07-12 21:58:58 -0500

    AWSMultiple-Draft

---
+++ awsmultiple/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+    version = "2.69.0"
+    region="us-east-1"
+}
+
+resource "aws_instance" "machine1" {
+    ami           = "ami-04b9e92b5572fa0d1"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1b"
+    tags = {
+      "type" = var.myTag
+    }
+}
+
+resource "aws_instance" "machine2" {
+    ami           = "ami-04b9e92b5572fa0d1"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1b"
+    tags = {
+      "type" = var.myTag
+    }
+}
+
+#resource "aws_network_interface_sg_attachment" "sg_attachment1" {
+#  security_group_id    = "sg-8e01a2fb"
+#  network_interface_id = "${aws_instance.machine1.primary_network_interface_id}"
+#}
+
+#resource "aws_network_interface_sg_attachment" "sg_attachment2" {
+#  security_group_id    = "sg-aa750ddc"
+#  network_interface_id = "${aws_instance.machine2.primary_network_interface_id}"
+#}
---
+++ awsmultiple/variables.tf
@@ -0,0 +1,10 @@
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "myTag" {
+  description = "My Input Tag"
+  default = "terraform-test"
+}
+
