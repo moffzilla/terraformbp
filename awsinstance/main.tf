4e02fb4ad94469bd1c73f1cbaede9d46a8fa11ac
Moff <jfrancisco.poo@gmail.com>
2020-08-07 11:09:32 -0500

    Setting Resource Region to west-1b

--- awsinstance/main.tf
+++ awsinstance/main.tf
@@ -16,7 +16,7 @@ variable "myTag" {
 resource "aws_instance" "machine1" {
     ami           = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
-    availability_zone = "us-east-1b"
+    availability_zone = "us-west-1b"
     tags = {
       "type" = var.myTag
     }
@@ -25,7 +25,7 @@ resource "aws_instance" "machine1" {
 resource "aws_instance" "machine2" {
     ami           = "ami-04b9e92b5572fa0d1"
     instance_type = "t2.micro"
-    availability_zone = "us-east-1b"
+    availability_zone = "us-west-1b"
     tags = {
       "type" = var.myTag
     }
