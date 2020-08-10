a1e1cd7ab195e53443dbb254f6b47b738e461da2
Moff <jfrancisco.poo@gmail.com>
2020-08-10 10:20:37 -0500

    Generic vSphere Endpoint test

--- vsphere/main.tf
+++ vsphere/main.tf
@@ -1,39 +1,39 @@
 provider "vsphere" {
-  user           = var.vsphere_user
-  password       = var.vsphere_password
-  vsphere_server = var.vsphere_server
+  #user           = var.vsphere_user
+  #password       = var.vsphere_password
+  #vsphere_server = var.vsphere_server
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
 
 data "vsphere_datacenter" "dc" {
-  name = "dc1"
+  name = "sc2dc03"
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "datastore1"
+  name          = "sc2c01vsan01"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_resource_pool" "pool" {
-  name          = "cluster1/Resources"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
+#data "vsphere_resource_pool" "pool" {
+#  name          = "cluster1/Resources"
+#  datacenter_id = data.vsphere_datacenter.dc.id
+#}
 
 data "vsphere_network" "network" {
-  name          = "public"
+  name          = "web"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
+  #resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
 
   num_cpus = 2
   memory   = 1024
-  guest_id = "other3xLinux64Guest"
+  guest_id = "Ubuntu-test"
 
   network_interface {
     network_id = data.vsphere_network.network.id
